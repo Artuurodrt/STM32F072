834268529e5a87f85414c490d76c17da21e6cfe0
DESKTOP-EB5LE0G\Arturo de los Rios <artuurodrt@gmail.com>
2022-03-31 21:25:03 -0600

    Updates that Set APB1 prescaler to /4 *( PCLK -> 12 MHz)

--- UART_Print/Debug/Src/subdir.mk
+++ UART_Print/Debug/Src/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -36,13 +36,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/%.o: ../Src/%.c Src/subdir.mk
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F0 -DSTM32F072RBTx -DNUCLEO_F072RB -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/printf-stdarg.d ./Src/printf-stdarg.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/system_stm32f0xx.d ./Src/system_stm32f0xx.o ./Src/systemclk_Config.d ./Src/systemclk_Config.o ./Src/uartPrint.d ./Src/uartPrint.o ./Src/userLed.d ./Src/userLed.o
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/printf-stdarg.d ./Src/printf-stdarg.o ./Src/printf-stdarg.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/system_stm32f0xx.d ./Src/system_stm32f0xx.o ./Src/system_stm32f0xx.su ./Src/systemclk_Config.d ./Src/systemclk_Config.o ./Src/systemclk_Config.su ./Src/uartPrint.d ./Src/uartPrint.o ./Src/uartPrint.su ./Src/userLed.d ./Src/userLed.o ./Src/userLed.su
 
 .PHONY: clean-Src
 
